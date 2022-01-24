7fa2b9972c11db1ae00e7a4ce89be479cf378a2a
MingLongSu <bananafrenzy31@gmail.com>
2022-01-24 01:08:33 -0500

    Added on-hover spinning logo to topbar

--- client/my-app/src/components/Topbar/Topbar.tsx
+++ client/my-app/src/components/Topbar/Topbar.tsx
@@ -1,9 +1,14 @@
-import './topbar.css'
+import "./topbar.css"
 
 const Topbar: React.FC = () => {
   return (
     <div className="Topbar">
+      <div className="Topbar__web-logo-container">
 
+      </div>
+      <div className="Topbar__meow-apps-logo-container">
+        <div className="meow-apps-logo-container__meow-apps-logo"></div>
+      </div>
     </div>
   );
 };
