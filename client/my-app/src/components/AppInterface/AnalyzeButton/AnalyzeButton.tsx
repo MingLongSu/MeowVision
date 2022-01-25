3f1343692f65939548778baea071e4006239e7db
MingLongSu <bananafrenzy31@gmail.com>
2022-01-24 23:12:57 -0500

    Designed general layout for app-interface

---
+++ client/my-app/src/components/AppInterface/AnalyzeButton/AnalyzeButton.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './analyzebutton.css';
+
+const AnalyzeButton: React.FC = () => {
+  return (
+      <div className="AnalyzeButton">
+          <div className="AnalyzeButton__analyze-button-container">
+              <div className="analyze-button-container__button">
+                  
+              </div>
+          </div>
+      </div>
+  );
+};
+
+export default AnalyzeButton;
--- client/my-app/src/components/AppInterface/AppInterface.tsx
+++ client/my-app/src/components/AppInterface/AppInterface.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import AnalyzeButton from './AnalyzeButton/AnalyzeButton';
 
 import './AppInterface.css';
+import Drawer from './Drawer/Drawer';
+import UploadPanel from './UploadPanel/UploadPanel';
 
 const AppInterface: React.FC = () => {
   return (
     <div className="AppInterface">
-        
+        <div className="AppInterface__analysis">
+          <AnalyzeButton> </AnalyzeButton>
+          <UploadPanel> </UploadPanel>
+        </div>
+        <div className="AppInterface__drawer-container">
+          <Drawer> </Drawer>
+        </div>
     </div>
   );
 };
