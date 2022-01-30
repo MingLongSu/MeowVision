25af9e44d3a4b836937e3a62e5961ca27611755f
MingLongSu <bananafrenzy31@gmail.com>
2022-01-30 03:28:21 -0500

    Small commit Fixed centering on the uploaded image

--- client/my-app/src/components/AppInterface/AppInterface.tsx
+++ client/my-app/src/components/AppInterface/AppInterface.tsx
@@ -6,15 +6,15 @@ import Drawer from './Drawer/Drawer';
 import UploadPanel from './UploadPanel/UploadPanel';
 
 const AppInterface: React.FC = () => {
-  //const [clickAnalyze, setClickAnalyze] = useState<boolean>(false);
   const [openDrawer, setOpenDrawer] = useState<boolean>(false);
   const [imageURL, setImageURL] = useState<string | null>(null);
+  const [urls, setURLs] = useState<string[]>([]);
 
   return (
     <div className="AppInterface">
-        <UploadPanel openDrawer={ openDrawer } imageURL={ imageURL } setImageURL={ setImageURL } > </UploadPanel>
+        <UploadPanel openDrawer={ openDrawer } imageURL={ imageURL } setImageURL={ setImageURL } urls={ urls } setURLs={ setURLs} > </UploadPanel>
         <div className="AppInterface__drawer-container">
-          <Drawer openDrawer={ openDrawer } setOpenDrawer={ setOpenDrawer }> </Drawer>
+          <Drawer openDrawer={ openDrawer } setOpenDrawer={ setOpenDrawer } urls={ urls } setImageURL={ setImageURL }> </Drawer>
         </div>
     </div>
   );
--- client/my-app/src/components/AppInterface/Drawer/Drawer.tsx
+++ client/my-app/src/components/AppInterface/Drawer/Drawer.tsx
@@ -5,17 +5,27 @@ import './drawer.css';
 interface DrawerState { 
   openDrawer: boolean;
   setOpenDrawer: React.Dispatch<React.SetStateAction<boolean>>;
+  urls: string[];
+  setImageURL: React.Dispatch<React.SetStateAction<string | null>>;
 }
 
-const Drawer: React.FC<DrawerState> = ({ openDrawer, setOpenDrawer }) => {
+const Drawer: React.FC<DrawerState> = ({ openDrawer, setOpenDrawer, urls, setImageURL }) => {
   const interactDrawer = () => { 
     setOpenDrawer(prevDrawerState => !prevDrawerState);
+  }
+
+  const getRandomKey = () => { 
+    return Math.floor(Math.random() * 1000);
   }
 
   return (
     <div className={ "Drawer" + (openDrawer ? " active" : "") }>
         <div className="Drawer__drawer-interior">
-
+          {
+            urls.map((imageURL) => { 
+              return <img onClick={ () => setImageURL(imageURL) } src={ imageURL } className="drawer__recent-image" key={ `${ getRandomKey() }` }></img>
+            })
+          }
         </div>
         <button onClick={ interactDrawer } className="Drawer__drawer-handle">
           <i className={ "fas fa-caret-left" + (openDrawer ? " active" : "")}></i>
